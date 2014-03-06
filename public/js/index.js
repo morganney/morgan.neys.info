87e27ee2c56b17e9cf729894052ef7bf6b237134
Morgan Ney <morganney@gmail.com>
2014-03-06 15:48:38 -0800

    round of dev

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+$(function() {
+  var $map = $('#map'),
+      map = new google.maps.Map($map.get(0), {
+        center: new google.maps.LatLng(37.668821, -122.080796),
+        zoom: 11
+      });
+
+
+});
