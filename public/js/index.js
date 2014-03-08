a73a69b073af2725473bea2afbd434b816e77765
Morgan Ney <morganney@gmail.com>
2014-03-08 15:37:26 -0800

    round of dev

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,56 @@
 $(function() {
   var $map = $('#map'),
-      map = new google.maps.Map($map.get(0), {
-        center: new google.maps.LatLng(37.668821, -122.080796),
-        zoom: 11
-      });
+      hayward = new google.maps.LatLng(37.668821, -122.080796),
+      sanjose = new google.maps.LatLng(37.339386, -121.894955),
+      berkeley = new google.maps.LatLng(37.871593, -122.272747),
+      oakland = new google.maps.LatLng(37.804364, -122.271114),
+      bounds = new google.maps.LatLngBounds(),
+      options = {
+        disableDefaultUI: true,
+        zoomControl: true,
+        zoomControlOptions: {
+          position:  google.maps.ControlPosition.TOP_LEFT,
+          style: google.maps.ZoomControlStyle.SMALL
+        },
+        styles: [
+          {
+            "stylers": [
+              { "gamma": 0.3 }
+            ]
+          }
+        ]
+      },
+      hmark = new google.maps.Marker({
+        position: hayward,
+        title: 'Hayward'
+      }),
+      smark = new google.maps.Marker({
+        position: sanjose,
+        title: 'San Jose'
+      }),
+      bmark = new google.maps.Marker({
+        position: berkeley,
+        title: 'Berkeley'
+      }),
+      omark = new google.maps.Marker({
+        position: oakland,
+        title: 'Oakland'
+      }),
+      map;
 
+  bounds.extend(hayward);
+  bounds.extend(sanjose);
+  bounds.extend(berkeley);
+  bounds.extend(oakland);
+
+  // It appears 'center' and 'zoom' are not required when using fitBounds()
+  map = new google.maps.Map($map.get(0),  options);
+
+  hmark.setMap(map);
+  smark.setMap(map);
+  bmark.setMap(map);
+  omark.setMap(map);
+
+  map.fitBounds(bounds);
 
 });
