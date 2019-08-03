2a42aaca9042deab7e2ac349af94bfa7533eb057
Morgan Ney <morganney@gmail.com>
2019-08-03 16:38:28 -0700

    remove vulns, update libs

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
-var express = require('express'),
-    routes = require('./routes'),
-    app = express();
+const express = require('express')
+const compression = require('compression')
+const errorhandler = require('errorhandler')
+const logger = require('morgan')
 
+const routes = require('./routes')
+
+const app = express()
 
 // Configuration
-app.configure('development', function() {
-  app.locals({
-    pretty: true,
-    isDev: true
-  });
-  app.use(express.logger('dev'));
-});
-app.configure('production', function() {
-  app.locals({isProd: true});
-  app.use(express.logger());
-});
-app.configure('all', function() {
-  app.enable('trust proxy');
-  app.set('view engine', 'jade');
-  app.locals({
-    title: 'Morgan Ney',
-    bodyClass: 'default'
-  });
-  app.use(express.compress());
-  app.use(app.router);
-  app.use(function(req, res, next) {
-    // TODO: 404 view
-    res.send(404);
-  });
-  app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.send(500);
-  });
-});
+if (app.get('env') === 'development') {
+  app.locals.pretty = true
+  app.locals.isDev = true
+  app.use(logger('dev'))
+  app.use(express.static('.'))
+} else {
+  app.locals.isProd = true
+  app.use(logger('combined'))
+}
+
+app.enable('trust proxy')
+app.set('view engine', 'pug')
+app.locals.title = 'Morgan Ney'
+app.locals.bodyClass = 'default'
+app.use(compression())
 
 // Routing
-app.get('/', routes.home);
-app.get('/robots.txt', routes.robots);
-app.get('/hobbies/motorcycles', routes.motorcycles);
-app.get('/hobbies/writing', routes.writing);
+app.get('/', routes.home)
+app.get('/robots.txt', routes.robots)
+app.get('/hobbies/motorcycles', routes.motorcycles)
+app.get('/hobbies/writing', routes.writing)
 
+// Error handling
+app.use(function(req, res, next) {
+  // TODO: 404 view
+  res.status(404)
+})
+app.use(function(err, req, res, next) {
+  console.error(err.stack)
+  res.status(500)
+})
 
-require('http').createServer(app).listen('3030', function() {
+app.listen('3030', function() {
   console.log('morgan.neys.info now listening on port 3030')
-});
+})
--- routes.js
+++ routes.js
@@ -1,22 +1,22 @@
 exports.home = function(req, res) {
-  res.locals({bodyClass: 'home'});
-  res.render('index');
-};
+  res.locals.bodyClass = 'home'
+  res.render('index')
+}
 
 exports.motorcycles = function(req, res) {
-  res.locals({title: "Morgan Ney's Motorcycle"});
-  res.render('motorcycles');
-};
+  res.locals.title = "Morgan Ney's Motorcycle"
+  res.render('motorcycles')
+}
 
 exports.writing = function(req, res) {
-  res.locals({title: "Morgan Ney's Writing"});
+  res.locals.title = "Morgan Ney's Writing"
   res.render('writing');
-};
+}
 
 exports.robots = function(req, res) {
   res.set({
     'Content-Type': 'text/plain',
     'Expires': new Date("1/1/2050").toUTCString()
-  });
-  res.send(200, 'User-agent: *\nDisallow: ');
-};
+  })
+  res.status(200).send('User-agent: *\nDisallow: ')
+}
